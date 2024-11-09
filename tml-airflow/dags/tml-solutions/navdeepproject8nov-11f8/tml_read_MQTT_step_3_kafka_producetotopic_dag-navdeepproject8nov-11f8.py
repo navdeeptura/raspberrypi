fba078257383700d897fd23127db4b6b4ce813b7
navdeeptura <navdeeptura@email>
2024-11-09 17:04:06 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/navdeepproject8nov-11f8/tml_read_MQTT_step_3_kafka_producetotopic_dag-navdeepproject8nov-11f8.py
+++ tml-airflow/dags/tml-solutions/navdeepproject8nov-11f8/tml_read_MQTT_step_3_kafka_producetotopic_dag-navdeepproject8nov-11f8.py
@@ -32,7 +32,7 @@ default_args = {
   'identifier' : 'TML solution',  
   'mqtt_broker' : 'f10fd264fefc4d72b3a1fffd529c94ed.s1.eu.hivemq.cloud', # <<<****** Enter MQTT broker i.e. test.mosquitto.org
   'mqtt_port' : '8883', # <<<******** Enter MQTT port i.e. 1883, 8883    (for HiveMQ cluster)
-  'mqtt_subscribe_topic' : '', # <<<******** enter name of MQTT to subscribe to i.e. tml/iot  
+  'mqtt_subscribe_topic' : 'tml/cybersecurity', # <<<******** enter name of MQTT to subscribe to i.e. tml/iot  
   'mqtt_enabletls': '0', # set 1=TLS, 0=no TLSS  
   'delay' : '7000', # << ******* 7000 millisecond maximum delay for VIPER to wait for Kafka to return confirmation message is received and written to topic
   'topicid' : '-999', # <<< ********* do not modify      
